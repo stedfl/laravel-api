cdd351ad9ce11b48b58233fc03e29dccf1830b33
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-01-19 15:06:24 +0100

    add modal to confirm deleting of choosen project

--- resources/views/admin/partials/delete-form.blade.php
+++ resources/views/admin/partials/delete-form.blade.php
@@ -1,8 +1,29 @@
-<form class="d-inline" action="{{ route('admin.projects.destroy', $project)  }}" method="POST"
-    onsubmit="return confirm('Confermi l\'eliminazione del progetto {{ $project->name }}?')">
-    @csrf
-    @method('DELETE')
-    <button class="btn btn-danger" title="delete" type="submit">
-        <i class="fa-solid fa-trash-can"></i>
-    </button>
-</form>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-{{ $project->id }}">
+    <i class="fa-solid fa-trash-can"></i>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="modal-{{ $project->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Eliminazione Progetto</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Confermi l'eliminazione del progetto <span class="fw-bolder text-capitalize">{{ $project->name }}</span>?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                <form class="d-inline" action="{{ route('admin.projects.destroy', $project) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger" title="delete" type="submit">
+                        Conferma
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
