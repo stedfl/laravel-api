d695dfdb415c4d223e115434d2c39665aebe0bca
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-01-30 18:11:35 +0100

    add query in index function to have technogies and type lists

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Technology;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index() {
-        $projects = Project::with(['type', 'technologies'])->paginate(5);
-        return response()->json(compact('projects'));
+        $projects = Project::with(['type', 'technologies'])->paginate(4);
+        $technologies = Technology::all();
+        $types = Type::all();
+        return response()->json(compact('projects', 'technologies', 'types'));
     }
 
     public function show($slug) {
