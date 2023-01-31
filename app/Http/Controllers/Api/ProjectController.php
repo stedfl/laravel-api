1370173c77eba9301d53d416fb954b2232b09922
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-01-31 19:06:40 +0100

    add pagination in all queries

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Project;
 use App\Models\Technology;
 use App\Models\Type;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -30,21 +31,25 @@ class ProjectController extends Controller
 
     public function search() {
         $search = $_GET['search'];
-        $projects = Project::where('name', 'like', "%$search%")->with(['type', 'technologies'])->get();
+        $projects = Project::where('name', 'like', "%$search%")->with(['type', 'technologies'])->paginate(4)->withQueryString();
         return response()->json(compact('projects'));
     }
 
     public function getByType($id) {
-        $projects = Project::where('type_id', $id)->with(['type', 'technologies'])->get();
+        $projects = Project::where('type_id', $id)->with(['type', 'technologies'])->paginate(4)->withQueryString();
         return response()->json(compact('projects'));
     }
 
     public function getByTechnology($id) {
-        $projects = [];
-        $technology = Technology::where('id', $id)->with('projects')->first();
-        foreach($technology->projects as $project) {
-            $projects[] = Project::where('id', $project->id)->with(['type', 'technologies'])->first();
-        }
+        // $projects = [];
+        // $technology = Technology::where('id', $id)->with('projects')->first();
+        // foreach($technology->projects as $project) {
+        //     $projects[] = Project::where('id', $project->id)->with(['type', 'technologies'])->first();
+        // }
+
+        $projects = Project::with(['type', 'technologies'])->whereHas('technologies', function(Builder $query) use($id) {
+            $query->where('technology_id', $id);
+        })->paginate(4);
 
         return response()->json(compact('projects'));
     }
