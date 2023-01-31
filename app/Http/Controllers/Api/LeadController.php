3cd43310e4040063d432e30c4c189b701453973e
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-01-31 17:08:03 +0100

    add data validation from client form and sent mail with data

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,11 +3,55 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\NewContact;
+use App\Models\Lead;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
-    public function store() {
+    public function store(Request $request) {
+        $data = $request->all();
+        $success = true;
+        $validator = Validator::make($data,
+            [
+                'name' => 'required|min:3|max:40',
+                'email' => 'required|email|min:5|max:255',
+                'object' => 'required|min:3|max:100',
+                'message' => 'required|min:3|max:4000'
+            ],
+            [
+                'name.required' => 'Il campo nome è obbligatorio',
+                'name.min' => 'Il campo nome richiede almeno :min caratteri',
+                'name.max' => 'Il campo nome consente al massimo :max caratteri',
+                'email.required' => 'Il campo e-mail è obbligatorio',
+                'email.email' => 'Il campo mail non è formattato correttamente',
+                'email.min' => 'Il campo e-mail richiede almeno :min caratteri',
+                'email.max' => 'Il campo e-mail consente al massimo :max caratteri',
+                'object.required' => 'Il campo oggetto è obbligatorio',
+                'object.min' => 'Il campo oggetto richiede almeno :min caratteri',
+                'object.max' => 'Il campo oggetto consente al massimo :max caratteri',
+                'message.required' => 'Il campo messaggio è obbligatorio',
+                'message.min' => 'Il campo messaggio richiede almeno :min caratteri',
+                'message.max' => 'Il campo messaggio consente al massimo :max caratteri',
+            ]
+        );
 
+        if($validator->fails()) {
+            $success = false;
+            $errors = $validator->errors();
+
+            return response()->json(compact('success', 'errors'));
+
+        } else {
+            $success = true;
+            $new_lead = new Lead();
+            $new_lead->fill($data);
+            $new_lead->save();
+            Mail::to('info-portfolio@sdf.it')->send(new NewContact($new_lead));
+
+            return response()->json(compact('success'));
+        }
     }
 }
--- resources/views/email/new-contact.blade.php
+++ resources/views/email/new-contact.blade.php
@@ -1,1 +1,5 @@
 <h1>Nuova Email</h1>
+<p>Nome: {{$lead->name}}</p>
+<p>Email: {{$lead->email}}</p>
+<p>Oggetto: {{$lead->object}}</p>
+<p>Messaggio: {{$lead->message}}</p>
