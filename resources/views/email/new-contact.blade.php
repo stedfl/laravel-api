17d30721d4c6ee57cdb30ac5e611742f9d7dd58e
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-01-31 15:44:11 +0100

    create model,controller,mail, migration and route for mail sending

---
+++ resources/views/email/new-contact.blade.php
@@ -0,0 +1,1 @@
+<h1>Nuova Email</h1>
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LeadController;
 use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -28,3 +29,5 @@ Route::namespace('Api')
         Route::get('/projectsbytype/{id}', [ProjectController::class, 'getByType']);
         Route::get('/projectsbytechnology/{id}', [ProjectController::class, 'getByTechnology']);
     });
+
+Route::post('/contacts', [LeadController::class, 'store']);
